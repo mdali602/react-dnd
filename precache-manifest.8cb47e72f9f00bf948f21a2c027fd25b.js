fc0dc15229b74b9209678c1265e6fc98f93005f2
mohammad_ali.eronmicro <mohammad.ali@eronmicro.com>
2020-01-28 17:18:46 +0530

    Updates

---
+++ precache-manifest.8cb47e72f9f00bf948f21a2c027fd25b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1145c257948d6e5dec1bd30c5a059bdf",
+    "url": "/react-dnd/index.html"
+  },
+  {
+    "revision": "797c9633f5efcd9d2123",
+    "url": "/react-dnd/static/css/main.424c42a2.chunk.css"
+  },
+  {
+    "revision": "e17b4fce799f2cbe330b",
+    "url": "/react-dnd/static/js/2.013f524e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-dnd/static/js/2.013f524e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "797c9633f5efcd9d2123",
+    "url": "/react-dnd/static/js/main.30add8d8.chunk.js"
+  },
+  {
+    "revision": "d8c810544609d5d2b623",
+    "url": "/react-dnd/static/js/runtime-main.62d07c1c.js"
+  }
+]);
